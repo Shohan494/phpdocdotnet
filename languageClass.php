e2f70c557bfa16dd8412604300db650145aef421
Shohan494 <shohan12224943@gmail.com>
2021-02-19 05:08:03 +0600

    using  and basic knowledge of using self

--- languageClass.php
+++ languageClass.php
@@ -9,8 +9,18 @@ class Language {
 
     public $name;
 
-    function whichLanguage(){
+    function whichLanguageWrong(){
         echo "This is " . $name . " Language";
+    }
+
+    function whichLanguageRight(){
+        //echo "This is " . $this->name . " Language";
+
+        var_dump($this);
+        // $this POINTS TO THE CLASS OBJECT
+
+        // TO MAKE THIS RIGHT, WE HAVE TO USE $this
+        // self is used to access static or class variables or methods and this is used to access non-static or object variables or methods.
     }
 }
 
@@ -18,3 +28,12 @@ $languageClassInstance = new Language();
 
 var_dump($languageClassInstance);
 print_r($languageClassInstance);
+
+// $languageClassInstance->whichLanguage;
+// Warning: Undefined property: Language::$whichLanguage in C:\laragon\www\2021\phpdocnet\languageClass.php on line 22
+
+// $languageClassInstance->whichLanguageWrong();
+// Warning: Undefined variable $name in C:\laragon\www\2021\phpdocnet\languageClass.php on line 13
+
+$languageClassInstance->whichLanguageRight();
+// Warning: Undefined variable $name in C:\laragon\www\2021\phpdocnet\languageClass.php on line 13
